d4dff97af6ce3021c6f54585fb1e71cc1ff69847
Da Xue <da@lessconfused.com>
2023-06-28 22:49:59 +0800

    aml-s905x-cc-v2: sync with aml-s905x-cc

--- bl33/board/amlogic/gxl_p212_v1_ddr4/firmware/scp_task/pwr_ctrl.c
+++ bl33/board/amlogic/gxl_p212_v1_ddr4/firmware/scp_task/pwr_ctrl.c
@@ -152,11 +152,13 @@ void get_wakeup_source(void *response, unsigned int suspend_from)
 {
 	struct wakeup_info *p = (struct wakeup_info *)response;
 	unsigned val;
+#ifdef CONFIG_BT_WAKEUP
 	struct wakeup_gpio_info *gpio;
 	unsigned i = 0;
+#endif
 
 	p->status = RESPONSE_OK;
-	val = (POWER_KEY_WAKEUP_SRC | AUTO_WAKEUP_SRC | REMOTE_WAKEUP_SRC |
+	val = (AUTO_WAKEUP_SRC | REMOTE_WAKEUP_SRC |
 	       ETH_PHY_WAKEUP_SRC | BT_WAKEUP_SRC);
 #ifdef CONFIG_CEC_WAKEUP
 	if (suspend_from != SYS_POWEROFF)
@@ -164,16 +166,6 @@ void get_wakeup_source(void *response, unsigned int suspend_from)
 #endif
 	p->sources = val;
 
-	/* Power Key: AO_GPIO[3]*/
-	gpio = &(p->gpio_info[i]);
-	gpio->wakeup_id = POWER_KEY_WAKEUP_SRC;
-	gpio->gpio_in_idx = GPIOAO_2;
-	gpio->gpio_in_ao = 1;
-	gpio->gpio_out_idx = -1;
-	gpio->gpio_out_ao = -1;
-	gpio->irq = IRQ_AO_GPIO0_NUM;
-	gpio->trig_type = GPIO_IRQ_FALLING_EDGE;
-	p->gpio_info_count = ++i;
 #ifdef CONFIG_BT_WAKEUP
 	gpio = &(p->gpio_info[i]);
 	gpio->wakeup_id = BT_WAKEUP_SRC;
