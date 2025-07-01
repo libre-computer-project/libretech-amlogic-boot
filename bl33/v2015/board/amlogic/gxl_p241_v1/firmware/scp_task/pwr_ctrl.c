0b08f221a10cc080a03f0bdaada1328b92c4ab74
Da Xue <da@libre.computer>
2025-07-01 19:15:32 -0400

    aml-s805x-ac: scp pwr_ctrl: check wakeup before print

--- bl33/v2015/board/amlogic/gxl_p241_v1/firmware/scp_task/pwr_ctrl.c
+++ bl33/v2015/board/amlogic/gxl_p241_v1/firmware/scp_task/pwr_ctrl.c
@@ -259,10 +259,12 @@ static unsigned int detect_key(unsigned int suspend_from)
 		}
 
 		if (irq[IRQ_AO_GPIO0] == IRQ_AO_GPIO0_NUM) {
+			// Test output immediately before printing and resetting interrupt
+			if ((readl(AO_GPIO_I) & (0x01 << 1)) == 0){
+				exit_reason = POWER_KEY_WAKEUP;
+			}
 			uart_puts("irq ao gpio0\n");
 			irq[IRQ_AO_GPIO0] = 0xFFFFFFFF;
-			if ((readl(AO_GPIO_I) & (1<<1)) == 0)
-				exit_reason = POWER_KEY_WAKEUP;
 		}
 		if (irq[IRQ_GPIO0] == IRQ_GPIO0_NUM) {
 			uart_puts("irq gpio0\n");
